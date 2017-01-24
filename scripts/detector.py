57816e23095a162c9b3a0fb41a41a4b8f589fcd0
Ali <ali@cataltepe.com>
2017-01-24 10:48:20 +0200

    updated detector stub

--- scripts/detector.py
+++ scripts/detector.py
@@ -1,1 +1,57 @@
 #!/usr/bin/env python
+# -*- coding:utf8 -*-
+import rospy, os, sys, curses, time, cv2, tf
+import numpy as np
+from numpy import deg2rad
+from curses import wrapper
+from threading import Thread
+from geometry_msgs.msg import Twist, Pose, PoseStamped, PoseWithCovariance, PoseWithCovarianceStamped, Transform
+from sensor_msgs.msg import LaserScan, Imu
+from metal_detector_msgs.msg._Coil import Coil
+from tf import transformations
+
+std = None
+
+radStep = deg2rad(15)
+linStep = 0.1
+transformer = None
+transListener = None
+
+
+coils = Coil()
+robotTwist = Twist()
+robotPose = PoseStamped()
+leftCoilPose = PoseStamped()
+rightCoilPose = PoseStamped()
+
+######################### AUXILIARY FUNCTIONS ############################
+
+# Get a transformation matrix from a geometry_msgs/Pose
+def matrix_from_pose_msg(pose):
+    t = transformations.translation_matrix((pose.position.x, pose.position.y, pose.position.z))
+    q = [pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w]
+    r = transformations.quaternion_matrix(q)
+    return transformations.concatenate_matrices(t, r)
+
+# Get a geometry_msgs/Pose from a transformation matrix
+def pose_msg_from_matrix(transformation):
+    msg = Pose()
+    msg.position.x = transformation[0][3]
+    msg.position.y = transformation[1][3]
+    msg.position.z = transformation[2][3]
+    q = transformations.quaternion_from_matrix(transformation)
+    msg.orientation.x = q[0]
+    msg.orientation.y = q[1]
+    msg.orientation.z = q[2]
+    msg.orientation.w = q[3]
+    return msg
+
+if __name__ == '__main__':
+    # Initialize client node
+    rospy.init_node('detector')
+
+    transListener = tf.TransformListener()
+
+    # Subscribing to all these topics to bring the robot or simulation to live data
+    rospy.Subscriber("/coils", Coil, receiveCoilSignal, queue_size = 1)
+    Thread(target = spin).start()
